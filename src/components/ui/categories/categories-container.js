e7a23ccb99b48e9a4c702fca93339f93e4dcab84
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2022-03-04 20:31:42 +0800

    fixed categories validation

--- src/components/ui/categories/categories-container.js
+++ src/components/ui/categories/categories-container.js
@@ -5,51 +5,53 @@ import { Fragment } from "react/cjs/react.development";
 import CategoriesList from "./categories-list";
 
 function CategoriesContainer({ name, labelClassName }) {
-	const [categories, setCategories] = useState();
-	const [categoriesChecked, setCategoriesChecked] = useState([]);
+  const [categories, setCategories] = useState();
+  const [categoriesChecked, setCategoriesChecked] = useState([]);
 
-	useEffect(() => {
-		getCategories()
-			.then(({ data }) => {
-				setCategories(data.categories);
-			})
-			.catch((error) => console.log(error));
-	}, []);
+  useEffect(() => {
+    getCategories()
+      .then(({ data }) => {
+        setCategories(data.categories);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-	return (
-		<Field name={name}>
-			{({ form, field, meta }) => {
-				console.log("CategoriesList -->", form, field, meta);
-				const handleCheckedCategories = () => {
-					form.setFieldTouched(field.name, true);
-					form.setFieldValue(field.name, categoriesChecked);
-				};
+  return (
+    <Field name={name}>
+      {({ form, field, meta }) => {
+        console.log("CategoriesList -->", form, field, meta);
+        const handleCheckedCategories = () => {
+          form.setFieldTouched(field.name, true);
+          form.setFieldValue(field.name, categoriesChecked);
+        };
 
-				const onCheckboxChange = (e) => {
-					const cboxName = e.target.name;
-					form.setFieldTouched(field.name, true);
-					if (e.target.checked) {
-						setCategoriesChecked([...categoriesChecked, cboxName]);
-					} else if (!e.target.checked) {
-						setCategoriesChecked(categoriesChecked.filter((c) => c !== e.target.name));
-					}
-				};
-				return (
-					<Fragment>
-						<CategoriesList
-							categories={categories}
-							labelClassName={labelClassName}
-							handleCheckedCategories={handleCheckedCategories}
-							onCheckboxChange={onCheckboxChange}
-						/>
-						<small className="text-danger d-block" style={{ height: "1rem" }}>
-							{meta.error && meta.touched ? meta.error : ""}
-						</small>
-					</Fragment>
-				);
-			}}
-		</Field>
-	);
+        const onCheckboxChange = (e) => {
+          const cboxName = e.target.name;
+          // form.setFieldTouched(field.name, true);
+          if (e.target.checked) {
+            setCategoriesChecked([...categoriesChecked, cboxName]);
+          } else if (!e.target.checked) {
+            setCategoriesChecked(
+              categoriesChecked.filter((c) => c !== e.target.name)
+            );
+          }
+        };
+        return (
+          <Fragment>
+            <CategoriesList
+              categories={categories}
+              labelClassName={labelClassName}
+              handleCheckedCategories={handleCheckedCategories}
+              onCheckboxChange={onCheckboxChange}
+            />
+            <small className="text-danger d-block" style={{ height: "1rem" }}>
+              {meta.error && meta.touched ? meta.error : ""}
+            </small>
+          </Fragment>
+        );
+      }}
+    </Field>
+  );
 }
 
 export default CategoriesContainer;
--- src/validation/add-package.js
+++ src/validation/add-package.js
@@ -2,29 +2,34 @@ import * as Yup from "yup";
 const { reach } = Yup;
 
 export const validationSchema = Yup.object({
-	title: Yup.string()
-		.required("Title is required.")
-		.min(3, "Must be at least 3 characters.")
-		.max(50, "Must not exceed 50 characters."),
-	description: Yup.string()
-		.required("Description is required.")
-		.min(50, "Must be at least 50 characters."),
-	maxCount: Yup.number().required("Group size is required."),
-	days: Yup.number().required("Days is required."),
-	nights: Yup.number().required("Nights is required."),
-	category: Yup.string().required("Category is required."),
-	salesPrice: Yup.number().required("Sales Price is required."),
-	regularPrice: Yup.number().required("Regular Price is required."),
-	discount: Yup.number().required("Discount is required."),
-	images: Yup.array().required("Images is required").min(3, "At least 3 images are required"),
-	programs: Yup.array().of(
-		Yup.object({
-			title: Yup.string().required("Title is required"),
-			// .max(50, "You have exceeded max number of characters"),
-			description: Yup.string().required("Description is required")
-			// .min(160, "At least 160 characters required")
-		})
-	),
-	keywords: Yup.array().min(1, "You need to have at least one keyword."),
-	categories: Yup.array().min(1, "You need to select at least one category.")
+  title: Yup.string()
+    .required("Title is required.")
+    .min(3, "Must be at least 3 characters.")
+    .max(50, "Must not exceed 50 characters."),
+  description: Yup.string()
+    .required("Description is required.")
+    .min(50, "Must be at least 50 characters."),
+  maxCount: Yup.number().required("Group size is required."),
+  days: Yup.number().required("Days is required."),
+  nights: Yup.number().required("Nights is required."),
+  category: Yup.string().required("Category is required."),
+  salesPrice: Yup.number().required("Sales Price is required."),
+  regularPrice: Yup.number().required("Regular Price is required."),
+  discount: Yup.number().required("Discount is required."),
+  images: Yup.array()
+    .required("Images is required")
+    .min(3, "At least 3 images are required"),
+  programs: Yup.array().of(
+    Yup.object({
+      title: Yup.string().required("Title is required"),
+      // .max(50, "You have exceeded max number of characters"),
+      description: Yup.string().required("Description is required")
+      // .min(160, "At least 160 characters required")
+    })
+  ),
+  keywords: Yup.array().min(1, "You need to have at least one keyword."),
+  categories: Yup.array().min(1, "You need to select at least one category."),
+  programIntro: Yup.string()
+    .required("Description is required.")
+    .min(50, "Must be at least 50 characters.")
 });
